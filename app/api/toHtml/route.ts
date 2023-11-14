1e48b60bea55c7452c55e237f6b37cc31ebf48a9
Anerco <aner.cohen@gmail.com>
2023-11-14 22:55:59 +0200

    refactor: use openai

--- app/api/toHtml/route.ts
+++ app/api/toHtml/route.ts
@@ -1,3 +1,9 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
 const systemPrompt = `You are an expert tailwind developer. A user will provide you with a
  low-fidelity wireframe of an application and you will return 
  a single html file that uses tailwind to create the website. Use creative license to make the application more fleshed out.
@@ -5,74 +11,37 @@ if you need to insert an image, use placehold.co to create a placeholder image. 
 
 export async function POST(request: Request) {
   const { image } = await request.json();
-  const body: GPT4VCompletionRequest = {
-    model: "gpt-4-vision-preview",
-    max_tokens: 4096,
-    messages: [
-      {
-        role: "system",
-        content: systemPrompt,
-      },
-      {
-        role: "user",
-        content: [
-          {
-            type: "image_url",
-            image_url: image,
-          },
-          "Turn this into a single html file using tailwind.",
-        ],
-      },
-    ],
-  };
 
-  let json = null;
   try {
-    const resp = await fetch("https://api.openai.com/v1/chat/completions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-      },
-      body: JSON.stringify(body),
+    const response = await openai.chat.completions.create({
+      model: "gpt-4-vision-preview",
+      max_tokens: 4096,
+      messages: [
+        {
+          role: "system",
+          content: systemPrompt,
+        },
+        {
+          role: "user",
+          content: [
+            {
+              type: "image_url",
+              image_url: image,
+            },
+            {
+              type: "text",
+              text: "Turn this into a single html file using tailwind.",
+            },
+          ],
+        },
+      ],
     });
-    json = await resp.json();
-  } catch (e) {
-    console.log(e);
+    return new Response(JSON.stringify(response), {
+      headers: {
+        "content-type": "application/json; charset=UTF-8",
+      },
+    });
+  } catch (error) {
+    console.error(error);
   }
-
-  return new Response(JSON.stringify(json), {
-    headers: {
-      "content-type": "application/json; charset=UTF-8",
-    },
-  });
 }
-
-type MessageContent =
-  | string
-  | (string | { type: "image_url"; image_url: string })[];
-
-export type GPT4VCompletionRequest = {
-  model: "gpt-4-vision-preview";
-  messages: {
-    role: "system" | "user" | "assistant" | "function";
-    content: MessageContent;
-    name?: string | undefined;
-  }[];
-  functions?: any[] | undefined;
-  function_call?: any | undefined;
-  stream?: boolean | undefined;
-  temperature?: number | undefined;
-  top_p?: number | undefined;
-  max_tokens?: number | undefined;
-  n?: number | undefined;
-  best_of?: number | undefined;
-  frequency_penalty?: number | undefined;
-  presence_penalty?: number | undefined;
-  logit_bias?:
-    | {
-        [x: string]: number;
-      }
-    | undefined;
-  stop?: (string[] | string) | undefined;
-};
