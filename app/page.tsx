b5acd25e19a6cb391efa0b2c0aa302847d6f3ad2
Steve Ruiz <steveruizok@gmail.com>
2023-11-15 21:47:21 +0000

    fix lockup styling

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import '@tldraw/tldraw/tldraw.css'
 import React, { useEffect, useState } from 'react'
 import { PreviewShapeUtil } from './PreviewShape/PreviewShape'
 import { ExportButton } from './components/ExportButton'
-import { useDialogs, useEditor } from '@tldraw/tldraw'
+import { useBreakpoint, useDialogs, useEditor } from '@tldraw/tldraw'
 import { APIKeyInput } from './components/APIKeyInput'
 
 const Tldraw = dynamic(async () => (await import('@tldraw/tldraw')).Tldraw, {
@@ -26,24 +26,25 @@ export default function Home() {
 					shareZone={<ExportButton />}
 				>
 					<APIKeyInput />
+					<LockupLink />
 				</Tldraw>
-				<a
-					href="https://www.tldraw.dev"
-					style={{
-						pointerEvents: 'all',
-						position: 'absolute',
-						zIndex: 100000,
-						bottom: 0,
-						right: 0,
-					}}
-				>
-					<img
-						className="lockup"
-						src="/lockup.svg"
-						style={{ padding: 8, height: 40 }}
-					/>
-				</a>
 			</div>
 		</>
+	)
+}
+
+function LockupLink() {
+	const breakpoint = useBreakpoint()
+	return (
+		<a
+			className={`lockup__link ${breakpoint < 4 ? 'lockup__link__mobile' : ''}`}
+			href="https://www.tldraw.dev"
+		>
+			<img
+				className="lockup"
+				src="/lockup.svg"
+				style={{ padding: 8, height: 40 }}
+			/>
+		</a>
 	)
 }
