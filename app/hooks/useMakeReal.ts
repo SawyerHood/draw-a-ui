a0ecfde8e3f1e1433fe64147eef7248743d6c352
Lu Wilson <l2wilson94@gmail.com>
2023-11-17 10:50:33 +0000

    analytics (#13)

--- app/hooks/useMakeReal.ts
+++ app/hooks/useMakeReal.ts
@@ -1,6 +1,7 @@
 import { useEditor, useToasts } from '@tldraw/tldraw'
 import { useCallback } from 'react'
 import { makeReal } from '../lib/makeReal'
+import { track } from '@vercel/analytics/react'
 
 export function useMakeReal() {
 	const editor = useEditor()
@@ -9,6 +10,8 @@ export function useMakeReal() {
 	return useCallback(async () => {
 		const input = document.getElementById('openai_key_risky_but_cool') as HTMLInputElement
 		const apiKey = input?.value ?? null
+		track('make_real', { timestamp: Date.now() })
+
 		try {
 			await makeReal(editor, apiKey)
 		} catch (e: any) {
