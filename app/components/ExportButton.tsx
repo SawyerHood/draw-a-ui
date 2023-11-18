b819f4759d7c8db4cab00899d04fa30987dc0eaf
Steve Ruiz <steveruizok@gmail.com>
2023-11-18 23:08:33 +0000

    fix button dead areas

--- app/components/ExportButton.tsx
+++ app/components/ExportButton.tsx
@@ -11,10 +11,12 @@ export function ExportButton() {
 	return (
 		<button
 			onClick={makeReal}
-			className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2"
+			className="p-2"
 			style={{ cursor: 'pointer', zIndex: 100000, pointerEvents: 'all' }}
 		>
-			Make Real
+			<div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+				Make Real
+			</div>
 		</button>
 	)
 }
