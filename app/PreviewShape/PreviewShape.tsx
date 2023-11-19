0811d7fb57359789add6f1ae534b50e497cfc0ef
Steve Ruiz <steveruizok@gmail.com>
2023-11-19 12:28:26 +0000

    style preview shape

--- app/PreviewShape/PreviewShape.tsx
+++ app/PreviewShape/PreviewShape.tsx
@@ -9,6 +9,7 @@ import {
 	useToasts,
 	DefaultSpinner,
 	stopEventPropagation,
+	Vec2d,
 } from '@tldraw/tldraw'
 
 export type PreviewShape = TLBaseShape<
@@ -20,6 +21,7 @@ export type PreviewShape = TLBaseShape<
 		h: number
 	}
 >
+
 export class PreviewShapeUtil extends BaseBoxShapeUtil<PreviewShape> {
 	static override type = 'preview' as const
 
@@ -42,6 +44,9 @@ export class PreviewShapeUtil extends BaseBoxShapeUtil<PreviewShape> {
 		const isEditing = useIsEditing(shape.id)
 		const toast = useToasts()
 
+		const pageRotation = this.editor.getShapePageTransform(shape)!.rotation()
+		const boxShadow = getRotatedBoxShadow(pageRotation)
+
 		// Kind of a hack—we're preventing user's from pinching-zooming into the iframe
 		const htmlToUse = shape.props.html
 			? shape.props.html.replace(
@@ -61,8 +66,8 @@ export class PreviewShapeUtil extends BaseBoxShapeUtil<PreviewShape> {
 						height={toDomPrecision(shape.props.h)}
 						draggable={false}
 						style={{
-							border: 0,
 							pointerEvents: isEditing ? 'auto' : 'none',
+							boxShadow,
 						}}
 					/>
 				) : (
@@ -74,14 +79,15 @@ export class PreviewShapeUtil extends BaseBoxShapeUtil<PreviewShape> {
 							display: 'flex',
 							alignItems: 'center',
 							justifyContent: 'center',
-							border: '1px solid var(--color-muted-1)',
+							boxShadow,
 						}}
 					>
 						<DefaultSpinner />
 					</div>
 				)}
-				<div
+				<button
 					style={{
+						all: 'unset',
 						position: 'absolute',
 						top: 0,
 						right: -40,
@@ -106,33 +112,36 @@ export class PreviewShapeUtil extends BaseBoxShapeUtil<PreviewShape> {
 					title="Copy code to clipboard"
 				>
 					<Icon icon="code" />
-				</div>
-				<div
-					style={{
-						textAlign: 'center',
-						position: 'absolute',
-						bottom: isEditing ? -40 : 0,
-						padding: 4,
-						fontFamily: 'inherit',
-						fontSize: 12,
-						left: 0,
-						width: '100%',
-						display: 'flex',
-						alignItems: 'center',
-						justifyContent: 'center',
-						pointerEvents: 'none',
-					}}
-				>
-					<span
+				</button>
+				{htmlToUse && (
+					<div
 						style={{
-							background: 'var(--color-background)',
-							padding: '4px 12px',
-							borderRadius: 99,
+							textAlign: 'center',
+							position: 'absolute',
+							bottom: isEditing ? -40 : 0,
+							padding: 4,
+							fontFamily: 'inherit',
+							fontSize: 12,
+							left: 0,
+							width: '100%',
+							display: 'flex',
+							alignItems: 'center',
+							justifyContent: 'center',
+							pointerEvents: 'none',
 						}}
 					>
-						{isEditing ? 'Click the canvas to exit' : 'Double click to interact'}
-					</span>
-				</div>
+						<span
+							style={{
+								background: 'var(--color-panel)',
+								padding: '4px 12px',
+								borderRadius: 99,
+								border: '1px solid var(--color-muted-1)',
+							}}
+						>
+							{isEditing ? 'Click the canvas to exit' : 'Double click to interact'}
+						</span>
+					</div>
+				)}
 			</HTMLContainer>
 		)
 	}
@@ -140,4 +149,33 @@ export class PreviewShapeUtil extends BaseBoxShapeUtil<PreviewShape> {
 	indicator(shape: PreviewShape) {
 		return <rect width={shape.props.w} height={shape.props.h} />
 	}
+}
+
+// todo: export these from tldraw
+
+const ROTATING_BOX_SHADOWS = [
+	{
+		offsetX: 0,
+		offsetY: 2,
+		blur: 4,
+		spread: 0,
+		color: '#00000029',
+	},
+	{
+		offsetX: 0,
+		offsetY: 3,
+		blur: 6,
+		spread: 0,
+		color: '#0000001f',
+	},
+]
+
+function getRotatedBoxShadow(rotation: number) {
+	const cssStrings = ROTATING_BOX_SHADOWS.map((shadow) => {
+		const { offsetX, offsetY, blur, spread, color } = shadow
+		const vec = new Vec2d(offsetX, offsetY)
+		const { x, y } = vec.rot(-rotation)
+		return `${x}px ${y}px ${blur}px ${spread}px ${color}`
+	})
+	return cssStrings.join(', ')
 }
