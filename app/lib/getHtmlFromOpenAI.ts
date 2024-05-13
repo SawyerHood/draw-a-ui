ecd4723ffc01629cbe96549b25772d8b623f6b25
Lu[ke] Wilson <l2wilson94@gmail.com>
2024-05-13 23:42:15 +0100

    4o

--- app/lib/getHtmlFromOpenAI.ts
+++ app/lib/getHtmlFromOpenAI.ts
@@ -99,7 +99,7 @@ export async function getHtmlFromOpenAI({
 	})
 
 	const body: GPT4VCompletionRequest = {
-		model: 'gpt-4-turbo',
+		model: 'gpt-4o',
 		max_tokens: 4096,
 		temperature: 0,
 		messages,
@@ -146,7 +146,7 @@ type MessageContent =
 	  )[]
 
 export type GPT4VCompletionRequest = {
-	model: 'gpt-4-turbo'
+	model: 'gpt-4o'
 	messages: {
 		role: 'system' | 'user' | 'assistant' | 'function'
 		content: MessageContent
