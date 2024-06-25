5d0367ea2475f261287c1dd9b46bfc8182315906
Steve Ruiz <steveruizok@gmail.com>
2024-06-25 10:01:08 +0100

    Update settings panel

--- app/components/ApiKeyDialog.tsx
+++ app/components/ApiKeyDialog.tsx
@@ -1,4 +1,3 @@
-import { useRef } from 'react'
 import {
 	TLUiDialogProps,
 	TldrawUiButton,
@@ -8,27 +7,36 @@ import {
 	TldrawUiDialogFooter,
 	TldrawUiDialogHeader,
 	TldrawUiDialogTitle,
+	TldrawUiIcon,
 	TldrawUiInput,
+	useReactor,
 	useValue,
 } from 'tldraw'
-import { makeRealSettings } from '../lib/settings'
+import { PROVIDERS, makeRealSettings } from '../lib/settings'
 import { SYSTEM_PROMPT } from '../prompt'
 
 export const ApiKeyDialog = ({ onClose }: TLUiDialogProps) => {
 	const settings = useValue('settings', () => makeRealSettings.get(), [])
-	const rRef = useRef<HTMLInputElement>(null)
+
+	useReactor(
+		'update settings local storage',
+		() => {
+			localStorage.setItem('makereal_settings_2', JSON.stringify(makeRealSettings.get()))
+		},
+		[]
+	)
 
 	return (
 		<>
 			<TldrawUiDialogHeader>
-				<TldrawUiDialogTitle>API Keys</TldrawUiDialogTitle>
+				<TldrawUiDialogTitle>Settings</TldrawUiDialogTitle>
 				<TldrawUiDialogCloseButton />
 			</TldrawUiDialogHeader>
 			<TldrawUiDialogBody
 				style={{ maxWidth: 350, display: 'flex', flexDirection: 'column', gap: 8 }}
 			>
 				<p>
-					To use Make Real, you will need to enter your API key for each provider you wish to use.{' '}
+					To use Make Real, enter your API key for each provider you wish to use.{' '}
 					<a
 						target="_blank"
 						href="https://tldraw.notion.site/Make-Real-FAQs-93be8b5273d14f7386e14eb142575e6e?pvs=4"
@@ -44,52 +52,34 @@ export const ApiKeyDialog = ({ onClose }: TLUiDialogProps) => {
 						className="apikey_select"
 						value={settings.provider}
 						onChange={(e) => {
-							const next = { ...settings, provider: e.target.value as any }
-							localStorage.setItem('makereal_settings_2', JSON.stringify(next))
-							makeRealSettings.set(next)
+							makeRealSettings.update((s) => ({ ...s, provider: e.target.value as any }))
 						}}
 					>
-						<option value="openai">OpenAI (gpt-4o)</option>
-						<option value="anthropic">Anthropic (Claude Sonnet 3.5)</option>
-						{/* <option value="google">Google (Gemeni 1.5 Pro)</option> */}
+						{PROVIDERS.map((provider) => {
+							return (
+								<option key={provider.id + 'option'} value={provider.id}>
+									{provider.name} {provider.model}
+								</option>
+							)
+						})}
 						<option value="all">All</option>
 					</select>
 				</div>
 				<hr style={{ margin: '12px 0px' }} />
-				<div
-					className="apikey_dialog"
-					ref={rRef}
-					style={{ display: 'flex', flexDirection: 'column', gap: 4 }}
-				>
-					<div style={{ display: 'flex', flexDirection: 'row', gap: 4 }}>
-						<label style={{ flexGrow: 2 }}>OpenAI</label>
-					</div>
-					<TldrawUiInput
-						className="apikey_input"
-						value={settings.keys.openai}
-						placeholder="risky but cool"
-						onValueChange={(value) => {
-							const next = { ...settings, keys: { ...settings.keys, openai: value } }
-							localStorage.setItem('makereal_settings_2', JSON.stringify(next))
-							makeRealSettings.set(next)
-						}}
-					/>
-				</div>
-				<div style={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
-					<div style={{ display: 'flex', flexDirection: 'row', gap: 4 }}>
-						<label style={{ flexGrow: 2 }}>Anthropic</label>
-					</div>
-					<TldrawUiInput
-						className="apikey_input"
-						value={settings.keys.anthropic}
-						placeholder="risky but cool"
-						onValueChange={(value) => {
-							const next = { ...settings, keys: { ...settings.keys, anthropic: value } }
-							localStorage.setItem('makereal_settings_2', JSON.stringify(next))
-							makeRealSettings.set(next)
-						}}
-					/>
-				</div>
+				{PROVIDERS.map((provider) => {
+					if (provider.id === 'google') return null
+					const value = settings.keys[provider.id]
+					return (
+						<ApiKeyInput
+							provider={provider}
+							key={provider.name + 'key'}
+							value={value}
+							warning={
+								value === '' && (settings.provider === provider.id || settings.provider === 'all')
+							}
+						/>
+					)
+				})}
 				{/* <div style={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
 					<div style={{ display: 'flex', flexDirection: 'row', gap: 4 }}>
 						<label style={{ flexGrow: 2 }}>Google</label>
@@ -112,12 +102,10 @@ export const ApiKeyDialog = ({ onClose }: TLUiDialogProps) => {
 						<button
 							style={{ all: 'unset', cursor: 'pointer' }}
 							onClick={() => {
-								const next = {
-									...settings,
-									prompts: { ...settings.prompts, system: SYSTEM_PROMPT },
-								}
-								localStorage.setItem('makereal_settings_2', JSON.stringify(next))
-								makeRealSettings.set(next)
+								makeRealSettings.update((s) => ({
+									...s,
+									prompts: { ...s.prompts, system: SYSTEM_PROMPT },
+								}))
 							}}
 						>
 							Reset
@@ -127,9 +115,7 @@ export const ApiKeyDialog = ({ onClose }: TLUiDialogProps) => {
 						className="apikey_input"
 						value={settings.prompts.system}
 						onValueChange={(value) => {
-							const next = { ...settings, prompts: { ...settings.prompts, system: value } }
-							localStorage.setItem('makereal_settings_2', JSON.stringify(next))
-							makeRealSettings.set(next)
+							makeRealSettings.update((s) => ({ ...s, prompts: { ...s.prompts, system: value } }))
 						}}
 					/>
 				</div>
@@ -145,5 +131,38 @@ export const ApiKeyDialog = ({ onClose }: TLUiDialogProps) => {
 				</TldrawUiButton>
 			</TldrawUiDialogFooter>
 		</>
+	)
+}
+
+function ApiKeyInput({
+	provider,
+	value,
+	warning,
+}: {
+	provider: (typeof PROVIDERS)[number]
+	value: string
+	warning: boolean
+}) {
+	const isValid = value.length === 0 || provider.validate(value)
+
+	return (
+		<div style={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
+			<div style={{ display: 'flex', flexDirection: 'row', gap: 4, alignItems: 'center' }}>
+				<label style={{ flexGrow: 2, color: warning ? 'red' : 'var(--color-text)' }}>
+					{provider.name} API key
+				</label>
+				<a style={{ cursor: 'pointer', pointerEvents: 'all' }} target="_blank" href={provider.help}>
+					<TldrawUiIcon className="apikey_help_icon" small icon="question-mark-circle" />
+				</a>
+			</div>
+			<TldrawUiInput
+				className={`apikey_input ${isValid ? '' : 'apikey_input__invalid'}`}
+				value={value}
+				placeholder="risky but cool"
+				onValueChange={(value) => {
+					makeRealSettings.update((s) => ({ ...s, keys: { ...s.keys, [provider.id]: value } }))
+				}}
+			/>
+		</div>
 	)
 }
--- app/lib/settings.tsx
+++ app/lib/settings.tsx
@@ -1,8 +1,26 @@
 import { atom } from 'tldraw'
 import { SYSTEM_PROMPT } from '../prompt'
 
+export const PROVIDERS = [
+	{
+		id: 'openai',
+		name: 'OpenAI',
+		model: 'GPT-4o',
+		help: 'https://tldraw.notion.site/Make-Real-Help-93be8b5273d14f7386e14eb142575e6e#a9b75e58b1824962a1a69a2f29ace9be',
+		validate: (key: string) => key.match(/sk-proj-[A-Za-z0-9]{32}/) !== null,
+	},
+	{
+		id: 'anthropic',
+		name: 'Anthropic',
+		model: 'Claude Sonnet 3.5',
+		help: 'https://tldraw.notion.site/Make-Real-Help-93be8b5273d14f7386e14eb142575e6e#3444b55a2ede405286929956d0be6e77',
+		validate: (key: string) => key.match(/sk-ant-api[0-9][0-9]-[A-Za-z0-9]{32}/) !== null,
+	},
+	// { id: 'google', name: 'Google', model: 'Gemeni 1.5 Flash', validate: (key: string) => true },
+]
+
 export const makeRealSettings = atom('make real settings', {
-	provider: 'openai' as 'openai' | 'anthropic' | 'google' | 'all',
+	provider: 'openai' as (typeof PROVIDERS)[number]['id'] | 'all',
 	keys: {
 		openai: '',
 		anthropic: '',
